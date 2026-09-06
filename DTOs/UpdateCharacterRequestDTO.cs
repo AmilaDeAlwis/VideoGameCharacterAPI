5575d594d7f5ea9e8b77adf66395bf2ca22707fb
AmilaDeAlwis <alwis.gimhana@gmail.com>
2026-09-06 16:11:41 +0530

    UpdateCharacterRequestDTO updated

--- DTOs/UpdateCharacterRequestDTO.cs
+++ DTOs/UpdateCharacterRequestDTO.cs
@@ -2,7 +2,6 @@
 {
     public class UpdateCharacterRequestDTO
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Game { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
