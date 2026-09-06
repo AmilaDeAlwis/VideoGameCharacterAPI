a92c76a4fd794538ebdfabe53d501703edff4c30
AmilaDeAlwis <alwis.gimhana@gmail.com>
2026-09-06 15:27:29 +0530

    Repositories updated

--- Services/IVideoGameCharacterService.cs
+++ Services/IVideoGameCharacterService.cs
@@ -1,13 +1,13 @@
-﻿using VideoGameCharacterAPI.Models;
+﻿using VideoGameCharacterAPI.DTOs;
 
 namespace VideoGameCharacterAPI.Services
 {
     public interface IVideoGameCharacterService
     {
-        Task<List<Character>> GetAllCharacterAsync();
-        Task<Character?> GetCharacterByIdAsync(int id);
-        Task<Character> AddCharacterAsync(Character character);
-        Task<bool> UpdateCharacterAsync(int id, Character character);
+        Task<List<CharacterResponseDTO>> GetAllCharacterAsync();
+        Task<CharacterResponseDTO?> GetCharacterByIdAsync(int id);
+        Task<CharacterResponseDTO> CreateCharacterAsync(CreateCharacterRequestDTO character);
+        Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequestDTO updatedCharacter);
         Task<bool> DeleteCharacterAsync(int id);
     }
 }
--- Services/VideoGameCharacterService.cs
+++ Services/VideoGameCharacterService.cs
@@ -1,37 +1,81 @@
-﻿using VideoGameCharacterAPI.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterAPI.Data;
+using VideoGameCharacterAPI.DTOs;
+using VideoGameCharacterAPI.Models;
 
 namespace VideoGameCharacterAPI.Services
 {
-    public class VideoGameCharacterService : IVideoGameCharacterService
+    public class VideoGameCharacterService(AppDbContext context) : IVideoGameCharacterService
     {
-        static List<Character> characters = new List<Character>{
-            new Character { Id = 1, Name = "Mario", Game = "Super Mario Bros.", Role = "Hero" },
-            new Character { Id = 2, Name = "Link", Game = "The Legend of Zelda", Role = "Hero" },
-            new Character { Id = 3, Name = "Bowser", Game = "Super Mario Bros.", Role = "Villain" },
-        };
-
-        public Task<Character> AddCharacterAsync(Character character)
+        public async Task<CharacterResponseDTO> CreateCharacterAsync(CreateCharacterRequestDTO character)
         {
-            throw new NotImplementedException();
+            var newCharacter = new Character
+            {
+                Name = character.Name,
+                Game = character.Game,
+                Role = character.Role
+            };
+            context.Characters.Add(newCharacter);
+            await context.SaveChangesAsync();
+            return new CharacterResponseDTO
+            {
+                Id = newCharacter.Id,
+                Name = newCharacter.Name,
+                Game = newCharacter.Game,
+                Role = newCharacter.Role
+            };
         }
 
-        public Task<bool> DeleteCharacterAsync(int id)
+        public async Task<bool> DeleteCharacterAsync(int id)
         {
-            throw new NotImplementedException();
+            var characterToDelete = await context.Characters.FindAsync(id);
+            if (characterToDelete is null)
+            {
+                return false;
+            }
+            context.Characters.Remove(characterToDelete);
+            await context.SaveChangesAsync();
+            return true;
         }
 
-        public async Task<List<Character>> GetAllCharacterAsync()
-            => await Task.FromResult(characters);
+        public async Task<List<CharacterResponseDTO>> GetAllCharacterAsync() 
+            => await context.Characters.Select(c => new CharacterResponseDTO
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Game = c.Game,
+                Role = c.Role
+            }).ToListAsync();
 
-        public async Task<Character?> GetCharacterByIdAsync(int id)
+        public async Task<CharacterResponseDTO?> GetCharacterByIdAsync(int id)
         {
-            var result = characters.FirstOrDefault(c => c.Id == id);
-            return await Task.FromResult(result);
+            var result = await context.Characters
+                .Where(c => c.Id == id)
+                .Select(c => new CharacterResponseDTO
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Game = c.Game,
+                    Role = c.Role
+                })
+                .FirstOrDefaultAsync();
+            return result;
         }
 
-        public Task<bool> UpdateCharacterAsync(int id, Character character)
+        public async Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequestDTO updatedCharacter)
         {
-            throw new NotImplementedException();
+            var characterToUpdate = await context.Characters.FindAsync(id);
+            if (characterToUpdate is null)
+            {
+                return false;
+            }
+
+            characterToUpdate.Name = updatedCharacter.Name;
+            characterToUpdate.Game = updatedCharacter.Game;
+            characterToUpdate.Role = updatedCharacter.Role;
+
+            await context.SaveChangesAsync();
+            return true;
         }
     }
 }
