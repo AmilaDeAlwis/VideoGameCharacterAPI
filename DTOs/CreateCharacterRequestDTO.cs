d2ea7ce7a142a7a68344c88013e8a82a7258f707
AmilaDeAlwis <alwis.gimhana@gmail.com>
2026-09-06 15:25:42 +0530

    New DTOs created

---
+++ DTOs/CreateCharacterRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoGameCharacterAPI.DTOs
+{
+    public class CreateCharacterRequestDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Game { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
---
+++ DTOs/UpdateCharacterRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace VideoGameCharacterAPI.DTOs
+{
+    public class UpdateCharacterRequestDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Game { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
