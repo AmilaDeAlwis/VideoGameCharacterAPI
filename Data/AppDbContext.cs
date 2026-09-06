ffd1f3c85fd7f351f3ad1a050f2fa3227098c132
AmilaDeAlwis <alwis.gimhana@gmail.com>
2026-09-06 15:24:47 +0530

    Db Context created to mograte the DB

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterAPI.Models;
+
+namespace VideoGameCharacterAPI.Data
+{
+    public class AppDbContext (DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Models.Character> Characters => Set<Character>();
+    }
+}
